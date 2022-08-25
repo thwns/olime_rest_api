8a5464af56003e2263d5bb69dcb8f06e25f5da37
thwns <74963459+thwns@users.noreply.github.com>
2022-08-25 20:50:22 +0900

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -144,6 +144,7 @@ AUTH_USER_MODEL = 'core.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_FILTER_BACKENDS': 'django_filters.rest_framework.DjangoFilterBackend',
 }
 
 SPECTACULAR_SETTINGS = {
